29b68f96e658b711ffa869e744e0aa947c4d62e7
elexpr <be2e@yandex.ru>
2021-01-02 03:49:09 +0300

    Added add new task feature

--- frontend/pvm_client/src/components/Task.vue
+++ frontend/pvm_client/src/components/Task.vue
@@ -1,39 +1,66 @@
 <template>
   <div class="task_info"
        v-bind:class="{
+        'task_pri_null' : $props.priority === null,
         'task_pri_high' : $props.priority === 1,
         'task_pri_medium' : $props.priority === 0,
         'task_pri_low' : $props.priority === -1
        }"
   >
-    <div class="task_title">{{ $props.title }}</div>
-    <div class="task_bar">
-      <span class="task_bar_play"
-            v-bind:class="{ 'sepia_filter' : is_play_enable }"
-            v-on:click="play( $props.title )"
-      >▶</span>
-      <span class="task_bar_stop"
-            v-bind:class="{ 'sepia_filter' : !is_play_enable }"
-            v-on:click="stop"
-      >⏸</span>
-    </div>
-    <div class="task_time_format">
-      {{ fancyTimeFormat( this.task_stamp ) }}
-    </div>
-    <div class="task_time">
-      <div
-        class="task_progress"
-        v-bind:style="{
-          width: (this.task_stamp / $props.time) * 100 + '%'
-        }"
-      >
+    <template v-if="$props.newest">
+      <div>
+        <input type="text" :style="{ borderColor: new_task.title ? '' : 'red' }" placeholder="Task title" v-model="new_task.title" />
       </div>
-    </div>
-    <div class="task_title" style="text-align: center">
-      {{ this.deadline_date }}
-      <br>
-      {{ this.deadline_time }}
-    </div>
+      <div v-if="$props.newest === true">
+        priority:<br>
+        Low: <input type="radio" :name="$props.id + '_priority'" v-model="new_task.priority" value="-1" checked/>
+        Middle: <input type="radio" :name="$props.id + '_priority'" v-model="new_task.priority" value="0" />
+        High: <input type="radio" :name="$props.id + '_priority'" v-model="new_task.priority" value="1">
+      </div>
+      <div>
+        <button type="button" class="button-save" :disabled="!( new_task.title && new_task.deadline_date && new_task.deadline_time )" v-on:click="save">SAVE</button>
+      </div>
+
+      <div>
+        <input type="date" placeholder="Deadline date" v-model="new_task.deadline_date" :min="today_date" />
+        <br>
+        <input type="time" placeholder="Deadline time" v-model="new_task.deadline_time" :min="today_time"/>
+      </div>
+    </template>
+    <template v-else>
+      <div class="task_title">
+        {{ $props.title }}
+      </div>
+      <div class="task_bar">
+        <span class="task_bar_play"
+              v-bind:class="{ 'sepia_filter' : is_play_enable }"
+              v-on:click="play( $props.title )"
+        >▶</span>
+        <span class="task_bar_stop"
+              v-bind:class="{ 'sepia_filter' : !is_play_enable }"
+              v-on:click="stop"
+        >⏸</span>
+      </div>
+
+      <div class="task_time_format">
+        {{ fancyTimeFormat( this.task_stamp ) }}
+      </div>
+      <div class="task_time">
+        <div
+          class="task_progress"
+          v-bind:style="{
+            width: (this.task_stamp / $props.time) * 100 + '%'
+          }"
+        >
+        </div>
+      </div>
+
+      <div class="task_title" style="text-align: center">
+        {{ this.deadline_date }}
+        <br>
+        {{ this.deadline_time }}
+      </div>
+    </template>
   </div>
 </template>
 
@@ -41,15 +68,23 @@
 export default {
   name: "Task",
   props: [
+    'id',
     'title',
     'deadline_date',
     'deadline_time',
     'stamp',
     'time',
-    'priority'
+    'priority',
+    'newest'
   ],
 
   methods : {
+    save: function() {
+      this.$emit(
+        'save_task',
+        this.new_task
+      )
+    },
     play : function( title ) {
       this.is_play_enable = true;
       this.task_timer = setInterval(
@@ -87,15 +122,45 @@ export default {
 
   data: function() {
     return {
+      new_task : {
+        id : this.id,
+        title : null,
+        priority : -1,
+        deadline_date: "1997-09-19",
+        deadline_time: "10:30",
+        newest : false
+      },
+
       is_play_enable : false,
       task_timer : null,
-      task_stamp : this.stamp
+      task_stamp : this.stamp,
+
+      today_date : new Date().toJSON().split('T')[0],
+      today_time : new Date().toJSON().split('T')[1],
     }
-  }
+  },
 }
 </script>
 
 <style scoped>
+  input {
+    border-radius: 5px;
+    text-align: center;
+  }
+
+  button {
+    border-radius: 5px;
+  }
+
+  .button-save {
+    background-color: aquamarine;
+  }
+
+  .task_pri_null {
+    background-color: #ffffff;
+    border: 2px dashed grey !important;
+  }
+
   .task_pri_high {
     background-color: #ff6486;
   }
--- frontend/pvm_client/src/components/Tasks.vue
+++ frontend/pvm_client/src/components/Tasks.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
-    <div v-for="task in tasks" :key="task.title">
+    <div v-for="task in tasks" :key="task.id">
       <task
+        :id="task.id"
         :title="task.title"
         :deadline_date="task.deadline_date"
         :deadline_time="task.deadline_time"
         :time="task.time"
         :stamp="task.stamp"
         :priority="task.priority"
+        :newest="task.newest"
+
         @start_task="start_task"
+        @save_task="save_task"
       />
     </div>
-    <div class="badge bg-success add-btn">+ADD</div>
+    <div class="badge bg-success add-btn" v-on:click="add_task">+ADD</div>
   </div>
 </template>
 
@@ -26,39 +30,82 @@ export default {
     'current_task'
   ],
   methods : {
+    save_task( new_task ) {
+      let new_task_indx = this.tasks.findIndex(
+        (e, i, a) => {
+          if ( e.id === new_task.id ) return "hello world";
+          return false;
+        }
+      );
+
+      this.$set(
+        this.tasks,
+        new_task_indx,
+        Object.assign(
+          {},
+          this.tasks,
+          new_task
+        )
+      );
+    },
+
     start_task( title ) {
       this.$emit(
         'set_task',
         title
       )
+    },
+    add_task() {
+      let task_object = {
+          id : this.tasks[ this.tasks.length - 1].id + 1,
+          title : '---',
+          deadline_date : '30.12.2020',
+          deadline_time : '00:00',
+          time : '6000',
+          stamp : '0',
+          priority : null,
+          newest : true
+      };
+
+      this.$set(
+        this.tasks,
+        this.tasks.length,
+        task_object
+      );
     }
   },
   data: function () {
     return {
       tasks : [
         {
+          id : 0,
           title : 'Test task #1',
           deadline_date : '30.12.2020',
           deadline_time : '00:00',
           time : '6000',
           stamp : '0',
           priority : 1,
+          newest : false
         },
         {
+          id : 1,
           title : 'Test task #2',
           deadline_date : '30.12.2020',
           deadline_time : '00:00',
           time : '6000',
           stamp : '50',
           priority : 0,
+          newest : false
         },
         {
+          id : 2,
           title : 'Test task #3',
           deadline_date : '30.12.2020',
           deadline_time : '00:00',
           time : '6000',
           stamp : '360',
           priority : -1,
+          newest : false
         },
       ]
     }
